4b2072dbffc14ba3d29773a3699e97b34be3c78a
Nai <bapdola304@gmail.com>
2020-07-21 22:07:16 +0700

    update count data part2

--- routes/part2.js
+++ routes/part2.js
@@ -12,12 +12,12 @@ router.get('/:level', function (req, res, next) {
 });
 
 router.get('/count/data', function (req, res, next) {
-  const part1DataJson = fs.readFileSync('./data/part1.json');
-  const part1DataFormat = JSON.parse(part1DataJson);
-  const { part1Data } = part1DataFormat;
-  const level1 = part1Data.filter(data => data.level == 1);
-  const level2 = part1Data.filter(data => data.level == 2);
-  const level3 = part1Data.filter(data => data.level == 3);
+  const part2DataJson = fs.readFileSync('./data/part2.json');
+  const part2DataFormat = JSON.parse(part2DataJson);
+  const { part2Data } = part2DataFormat;
+  const level1 = part2Data.filter(data => data.level == 1);
+  const level2 = part2Data.filter(data => data.level == 2);
+  const level3 = part2Data.filter(data => data.level == 3);
   const data = {
     1: level1.length,
     2: level2.length,
